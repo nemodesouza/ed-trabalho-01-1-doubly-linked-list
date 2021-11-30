8530b7741bb2e8c9e5c99bb9e384e24b79304810
Nemo de Souza <nemodesouza@gmail.com>
2021-11-29 22:41:27 -0300

    instructions of the activity added

--- doublyLinkedList.py
+++ doublyLinkedList.py
@@ -1,2 +1,15 @@
 class DoublyLinkedList:
     pass
+
+# - [ ]  (elemento) acessarAtual()
+# - [ ]  (void) InserirAntesDoAtual ( novo )
+# - [ ]  (void) InserirApósAtual ( novo )
+# - [ ]  (void) inserirNoFim ( novo )
+# - [ ]  (void) inserirNaFrente ( novo )
+# - [ ]  (void) inserirNaPosicao ( k, novo )
+# - [ ]  (void) ExcluirAtual ()
+# - [ ]  (void) ExcluirPrim ()
+# - [ ]  (void) ExcluirUlt ()
+# - [ ]  (void) ExcluirElem ( chave )
+# - [ ]  (void) ExcluirDaPos ( k )
+# - [ ]  (boolean) Buscar ( chave )
