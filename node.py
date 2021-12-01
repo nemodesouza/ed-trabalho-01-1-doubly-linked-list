b779858cfadf79607ece6e7f29856b581445a333
Nemo de Souza <nemodesouza@gmail.com>
2021-12-01 10:23:17 -0300

    update of node methods

--- node.py
+++ node.py
@@ -3,3 +3,29 @@ class Node:
         self.__data = data
         self.__previous = None
         self.__next = None
+
+@property
+def data(self):
+    return self.__data
+
+@data.setter
+def data(self, data):
+    self.__data = data
+
+
+@property
+def previous(self):
+    return self.__previous
+
+@previous.setter
+def previous(self, previous):
+    self.__previous = previous
+
+@property
+def next(self):
+    return self.__next
+
+@next.setter
+def next(self, next):
+    self.__next = next
+
