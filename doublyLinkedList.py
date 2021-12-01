e120c77ae06c29cbd01cd49349f1c50782770fa0
Nemo de Souza <nemodesouza@gmail.com>
2021-12-01 11:54:32 -0300

    doubly linked list init, property and accessCurrent methods implemented

--- doublyLinkedList.py
+++ doublyLinkedList.py
@@ -1,7 +1,21 @@
-class DoublyLinkedList:
-    pass
+from cursor import Cursor
+from node import Node
 
-# - [ ]  (elemento) acessarAtual()
+class DoublyLinkedList:
+    def __init__(self, limit=None):
+        self.__cursor = Cursor()
+        self.__limit = None
+        self.__elements = 0
+
+    @property
+    def element(self):
+        return self.__elements
+
+    def accessCurrent(self):
+        """(elemento) acessarAtual()"""
+        return self__cursor.current
+
+# - [ ]  
 # - [ ]  (void) InserirAntesDoAtual ( novo )
 # - [ ]  (void) InserirApósAtual ( novo )
 # - [ ]  (void) inserirNoFim ( novo )
