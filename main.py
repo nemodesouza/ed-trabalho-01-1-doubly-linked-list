156a540eb8b378aec51ab57cea2ce55bfd174748
Nemo de Souza <nemodesouza@gmail.com>
2021-12-04 20:20:29 -0300

    begining of the tests

---
+++ main.py
@@ -0,0 +1,16 @@
+from doublyLinkedList import DoublyLinkedList
+
+if __name__ == '__main__':
+    
+    print(">>> Iniciando os testes")
+    print("")
+    print("")
+    print("")
+    print(">>> Criando uma Lista Duplamente Encadeada...")
+    dll = DoublyLinkedList()
+    print("Lista criada!")
+    print("")
+    print("")
+    print(">>> Criando uma Lista Duplamente Encadeada...")
+    dll = DoublyLinkedList()
+    print("Lista criada!")
